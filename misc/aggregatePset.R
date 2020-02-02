872ac5b39e44f1a6097db0a788e6b23541f116ca
Rene Quevedo <rquevedo@uhnresearch.ca>
2020-02-02 11:10:41 -0500

    Added support for aggregating gene psets, which differs from the standard bins or tads since the rows aren't perfectly ordered

--- misc/aggregatePset.R
+++ misc/aggregatePset.R
@@ -1,17 +1,89 @@
 library(Biobase)
-dataset <- 'CCLE'
+dataset <- 'GNE'
 pdir <- '/mnt/work1/users/pughlab/projects/cancer_cell_lines'
 pset.dir <- file.path(pdir, dataset, "eacon", "out", "PSet")
+
+###################
+#### FUNCTIONS ####
+combinePsets <- function(psets){
+  ## Uses the BiocGenerics::combine function to merge psets
+  while(length(psets) > 1){
+    print(length(psets))
+    pset1 <- lapply(seq(1, length(psets), by=2), function(pidx){
+      if((pidx + 1) > length(psets)){
+        psets[[pidx]]
+      } else {
+        combine(psets[[pidx]], psets[[pidx+1]])
+      }
+    })
+    psets <- pset1
+    rm(pset1); gc()
+  }
+  return(psets)
+}
+
+roundAssayData <- function(pset, sigdig=2){
+  assay.d <- ls(assayData(pset))
+  for(a in assay.d){
+    assayData(pset)[[a]] <- round(assayData(pset)[[a]], sigdig)
+  }
+  return(pset)
+}
+
+combinePsetsLomem <- function(all.ps){
+  assay.d <- ls(assayData(all.ps[[1]]))
+  while(length(all.ps) > 1){
+    print(paste0(length(all.ps), "..."))
+    for(a in assay.d){
+      assayData(all.ps[[1]])[[a]] <- cbind(assayData(all.ps[[1]])[[a]],
+                                           assayData(all.ps[[2]])[[a]])
+    }
+    phenoData(all.ps[[1]])@data <- rbind(phenoData(all.ps[[1]])@data,
+                                         phenoData(all.ps[[2]])@data)
+    all.ps <- all.ps[-2]
+  }
+  return(all.ps[[1]])
+}
+
+###############################
+#### Combine Bins and TADs ####
 for(data.type in c('bins', 'tads')){
   print(data.type)
-  all.files <- list.files(pset.dir, pattern=paste0(data.type, ".*RData"))
-  splits <- seq(1, length(all.files), by=10)
-  splits.df <- data.frame("start"=splits,
-                          "end"=c(splits[-1]-1, length(all.files)))
+  all.files <- list.files(pset.dir, pattern=paste0(data.type, ".*[0-9].*RData"))
+  idx  <- seq(1, length(all.files), by=5)
+  se.df <- data.frame("start"=idx,
+                      "end"=c(idx[-1]-1, length(all.files)))
   
-  all.psets <- apply(splits.df, 1, function(idx){
-    print(paste(idx, collapse="-"))
-    
+  all.ps <- apply(se.df, 1, function(i, round.psets=FALSE){
+    psets <- lapply(all.files[i['start']:i['end']], function(file){
+      load(file.path(pset.dir, file))
+      eset <- switch(data.type,
+                     gene=gene.eset,
+                     bins=out.eset,
+                     tads=out.eset)
+      return(eset)
+    })
+    psets <- combinePsetsLomem(psets)
+    if(round.psets) psets <- roundAssayData(psets)
+    return(psets)
+  })
+  pset <- combinePsetsLomem(all.ps) 
+
+  rm(all.ps)
+  save(pset, file=file.path(pset.dir, paste0(dataset, "_CN.", data.type, ".rda")))
+  saveRDS(pset, file=file.path(pset.dir, paste0(dataset, "_CN.", data.type, ".RDS")))
+}
+
+#######################
+#### Combine Genes ####
+for(data.type in c('gene')){
+  print(data.type)
+  all.files <- list.files(pset.dir, pattern=paste0(data.type, ".*[0-9].*RData"))
+  idx  <- seq(1, length(all.files), by=5)
+  se.df <- data.frame("start"=idx,
+                      "end"=c(idx[-1]-1, length(all.files)))
+  
+  all.ps <- apply(se.df, 1, function(idx, round.psets=FALSE){
     psets <- lapply(all.files[idx['start']:idx['end']], function(file){
       load(file.path(pset.dir, file))
       eset <- switch(data.type,
@@ -20,25 +92,31 @@ for(data.type in c('bins', 'tads')){
                      tads=out.eset)
       return(eset)
     })
+    
+    
     pset1 <- Reduce(function(x,y) combine(x,y), psets)
     rm(psets)
     return(pset1)
   })
-  save(all.psets, file=file.path(pset.dir, paste0(dataset, "_CN.", data.type, ".rda")))
+  save(all.ps, file=file.path(pset.dir, paste0(dataset, "_CN.", data.type, ".rda")))
   gc()
   
   max.comb.size <- 5
   psets <- list(); i <- 1
-  while(length(all.psets) > 0){
-    if(length(all.psets) < max.comb.size) max.comb.size <- length(all.psets)
-    psets[[i]] <- Reduce(function(x,y) combine(x,y), all.psets[1:max.comb.size])
-    all.psets <- all.psets[-c(1:max.comb.size)]
+  while(length(all.ps) > 0){
+    if(length(all.ps) < max.comb.size) max.comb.size <- length(all.ps)
+    psets[[i]] <- Reduce(function(x,y) combine(x,y), all.ps[1:max.comb.size])
+    all.ps <- all.ps[-c(1:max.comb.size)]
     i <- i+1
     gc()
   }
-  rm(all.psets)
+  rm(all.ps)
   pset <- Reduce(function(x,y) combine(x,y), psets)
   rm(psets)
   
   saveRDS(pset, file=file.path(pset.dir, paste0(dataset, "_CN.", data.type, ".RDS")))
 }
+
+
+
+
--- misc/runEacon.R
+++ misc/runEacon.R
@@ -33,7 +33,7 @@ option_list <- list(
               help="Index of sample group [default= %default]", metavar="integer"),
   make_option(c("-g", "--grpsize"), type="integer", default=10,
               help="Size of the groups to run in one job [default= %default]", metavar="integer"),
-  make_option(c("-d", "--dataset"), type="character", default='GDSC',
+  make_option(c("-d", "--dataset"), type="character", default='CCLE',
               help="Dataset to use, either 'GDSC' or 'GNE' or 'CCLE'"),
   make_option(c("-p", "--pdir"), type="character", default='/mnt/work1/users/pughlab/projects/cancer_cell_lines',
               help="Parent directory path"))
@@ -115,7 +115,7 @@ opt <- parse_args(opt_parser)
 
 
 segmenter <- 'ASCAT'
-dataset <- opt$dataset  #'GDSC', 'CCLE', 'gCSI'
+dataset <- opt$dataset  #'GDSC', 'CCLE', 'GNE'
 pdir <- file.path(opt$pdir, dataset)
 
 ## Normalization
@@ -284,7 +284,7 @@ for(segmenter in c("ASCAT")){
                                   bin.size=5000)
       
       cbio.path=file.path("out", "cBio")
-      # buildCbioOut(gr.cnv, cbio.path="./out/cBio")
+      buildCbioOut(gr.cnv, cbio.path="./out/cBio")
       
       ## Build standard bin and gene PSets
       pset.path=file.path("out", "PSet")
