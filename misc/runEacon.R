f6cab65d55906539c0675e55d383e920a2fea711
Rene Quevedo <rquevedo@uhnresearch.ca>
2020-06-16 21:14:09 -0400

    Added a lot of code documentation to the runEacon.R script, no big changes to any of the actual coding though

--- misc/runEacon.R
+++ misc/runEacon.R
@@ -12,11 +12,19 @@ if(install.pkg){
   ## Install the most recent STABLE version (@master)
   #devtools::install_github("gustaveroussy/EaCoN")
   
+  # The affy.CN.norm package provides pre-computed GC% and wave-effect 
+  # (re)normalization datasets for all compatible Affymetrix designs, 
+  # for both NA33/NA35 (hg19) and NA36 (hg38) human genome builds.
   install.packages("https://partage.gustaveroussy.fr/pydio_public/083305?dl=true&file=/affy.CN.norm.data_0.1.2.tar.gz", repos = NULL, type = "source")
-  devtools::install_github("gustaveroussy/apt.snp6.1.20.0")
-  install.packages("https://partage.gustaveroussy.fr/pydio_public/152397?dl=true&file=/GenomeWideSNP.6.na35.r1_0.1.0.tar.gz", repos = NULL, type = "source")
-  install.packages( "https://partage.gustaveroussy.fr/pydio_public/e6fe22?dl=true&file=/rcnorm_0.1.5.tar.gz", repos = NULL, type = "source")
   
+  # Contains the embedded APT tool
+  devtools::install_github("gustaveroussy/apt.snp6.1.20.0")
+  
+  # Annotations for SNP6.0
+  install.packages("https://partage.gustaveroussy.fr/pydio_public/152397?dl=true&file=/GenomeWideSNP.6.na35.r1_0.1.0.tar.gz", repos = NULL, type = "source")
+  
+  # Rawcopy subset for BAF normalization
+  install.packages( "https://partage.gustaveroussy.fr/pydio_public/e6fe22?dl=true&file=/rcnorm_0.1.5.tar.gz", repos = NULL, type = "source")
 }
 
 ##############
@@ -26,6 +34,7 @@ if(install.pkg){
 library(optparse)
 library(EaCoN)
 library(parallel)
+library(foreach)
 library(Biobase)
 library(foreach)
 
@@ -43,7 +52,7 @@ opt <- parse_args(opt_parser)
 
 
 .splitSamples <- function(sample.ids, range.idx, grp.size=10){
-  if((grp.size * range.idx) > length(sample.ids)){
+  if(((grp.size * range.idx) - grp.size) > length(sample.ids)){
     NULL 
   } else {
     if(grp.size > 1){
@@ -114,20 +123,22 @@ opt <- parse_args(opt_parser)
 }
 
 
-# opt$pdir <- '/mnt/work1/users/pughlab/projects/cancer_cell_lines'
-# opt$dataset <- '1000G'
+##################
+#### 0) Setup ####
 segmenter <- 'ASCAT'
-dataset <- opt$dataset  #'GDSC', 'CCLE', 'GNE'
+cl <- 10 # Number of cores
+run_parallel <- FALSE 
+dataset <- opt$dataset  #'GDSC', 'CCLE', 'GNE', '1000G'
 pdir <- file.path(opt$pdir, dataset)
-
-## Normalization
-# Outputs a ./YT_4941/YT_4941_GenomeWideSNP_6_hg19_processed.RDS file
-message(paste0("Searching for CEL$ files in: ", file.path(pdir, "data")))
 CEL.dir <- file.path(pdir, "data")
+
+message(paste0("Searching for CEL$ files in: ", file.path(pdir, "data")))
 sample.paths <- list.files(CEL.dir, pattern="CEL$", recursive = T, 
                          ignore.case = T, full.names = T)
-#sample.paths <- sample.paths[1:200]
-sample.ids <- gsub(".cel", "", basename(sample.paths), ignore.case = TRUE)
+if(any(grepl("symlinks", c("a", "b", "symlinks")))) sample.paths <- sample.paths[-grep("symlinks", sample.paths)]
+
+sample.ids <- gsub(".cel$", "", basename(sample.paths), ignore.case = TRUE)
+data.dir <- dirname(sample.paths)
 regm <- regexpr(".cel", basename(sample.paths), ignore.case=T)
 cel.suffix <- regmatches(x = basename(sample.paths), m = regm)
 
@@ -135,68 +146,76 @@ cel.suffix <- regmatches(x = basename(sample.paths), m = regm)
 dir.create(file.path(pdir, "eacon"), recursive = TRUE)
 setwd(file.path(pdir, "eacon"))
 
+#################################
+#### 1) Signal Normalization ####
+# For Affy6 analyses, this pipeline uses the 'rawcopy' method of
+# signal normalization. 
+#   APT:    v1.20.0
+#   Genome build:   na35.r1
+# Using the apt.snp6.1.20.0::apt.snp6.process() function, 
+# this creates an intermediatry CNCHP file using the apt-copynumber-workflow
+# in the APT toolkit (https://www.affymetrix.com/support/developer/powertools/changelog/apt-copynumber-workflow.html)
+# This is then converted into a OSCHP format for standardization purposes using
+# the apt2-dset-util APT tool (https://media.affymetrix.com/support/developer/powertools/changelog/apt2-dset-util.html).
+# This function employs the  standard APT tools which are embedded 
+# in the apt.snp6.1.20.0 R package, where the annotations are found in the
+# GenomeWideSNP.6.na35.r1_0.1.0 R package.
+#
+# The OSCHP is read in to the rcnorm package where after a little metadata
+# addition, the BAF is normalized using rcnorm::rcnorm.snp(). The L2R
+# undergoes wave and GC-normalization using EaCoN:::renorm.go() function, 
+# which uses the pre-computed GC% and wave-effect (re)normalization datasets 
+# stored in the affy.CN.norm.data_0.1.2 package. Post-normalized L2Rs are then
+# median centered.
+#
+# Finally, an ASCAT-like object is built to standardize the analyses for all 
+# subsequent steps between different data types.
 
-
-qsub.split <- FALSE
-if(qsub.split){
-  if(file.exists(file.path("..", "scripts", "samples.RData"))){
-    EaCoN:::tmsg("Getting sample IDs...")
-    load(file.path("..", "scripts", "samples.RData"))
-    range.s <- seq(1, length(sample.ids), by=10)
-    range.e <- c((range.s + 9), length(sample.ids))
-    range.idx <- opt$idx
-
-    range.se <- c(range.s[range.idx], range.e[range.idx])
-    print(paste0(range.idx, " : ", paste(range.se, collapse="-")))
-
-    sample.ids <- sample.ids[range.se[1]:range.se[2]]
-    #sample.ids <- sample.ids[opt$idx]
-  } else {
-    # fit.vals.path <- list.files(pattern="gammaEval.txt", recursive=T, full=T)
-    # rm.idx <- unlist(sapply(sample.ids, function(x) any(grepl(x, fit.vals.path))))
-    
-    rm.idx <- sapply(sample.ids, function(s){
-      if(dir.exists(s)){
-        if(any(grepl("temp", list.files(s)))){
-          print(paste("Incomplete sample [temp present]:", s))
-          unlink(file.path(s), recursive = TRUE)
-          temp.idx <- FALSE
-        } else if(any(grepl("RDS$", list.files(s)))) {
-          temp.idx <- TRUE
-        } else {
-          print(paste("Incomplete sample [RDS missing]:", s))
-          unlink(file.path(s), recursive = TRUE)
-          FALSE
-        }
-      } else {
-        temp.idx <- FALSE
-      }
-    })
-    
-    #names(rm.idx) <- sample.ids
-    if(sum(rm.idx) > 0) sample.ids <- sample.ids[-which(rm.idx)]
-    #sample.ids <- sample.ids[sample(x = c(1:length(sample.ids)), size = length(sample.ids), replace = F)]
-    
-    EaCoN:::tmsg("Saving samples...")
-    save(sample.ids, file=file.path("..", "scripts", "samples.RData"))
-  }
-}
-
+# sample.ids <- setdiff(sample.ids, list.files())
 if(dataset=='GNE'){
   illumina.dir <- file.path(pdir, 'illumina', 'GNE_Matrices')
   EaCoN::Build.OMNI25(illumina.dir=illumina.dir, parent.dir=pdir)
 } else {
-  mclapply(sample.ids, function(sample, sample.paths){
-    idx <- grep(sample, basename(sample.paths))
-    EaCoN:::SNP6.Process(CEL = file.path(CEL.dir, paste0(sample, cel.suffix[idx])), 
-                 samplename = sample)
-  }, sample.paths=sample.paths, mc.cores = 4)
+  if(run_parallel){
+    mclapply(sample.ids, function(sample){
+      setwd(file.path(pdir, "eacon"))
+      print(paste0(sample, ": (", match(sample, sample.ids), "/", 
+                   length(sample.ids), ")"))
+      tryCatch({
+        idx <- grep(sample, basename(sample.paths))
+        EaCoN:::SNP6.Process(CEL = file.path(data.dir[idx], paste0(sample, cel.suffix[idx])), 
+                             samplename = sample)
+      }, error=function(e){NULL})
+    }, mc.cores = cl)
+  } else {
+    for(sample in sample.ids){
+      print(paste0(sample, ": (", match(sample, sample.ids), "/", 
+                   length(sample.ids), ")"))
+      tryCatch({
+        setwd(file.path(pdir, "eacon"))
+        idx <- grep(sample, basename(sample.paths))
+        EaCoN:::SNP6.Process(CEL = file.path(data.dir[idx], paste0(sample, cel.suffix[idx])), 
+                             samplename = sample)
+      }, error=function(e){NULL})
+    }
+  }
+  
 }
 
 
-
-#### L2R Segmentation: ####
-# Takes in the _processed.RDS file 
+##############################
+#### 2) L2R Segmentation: ####
+# Takes in the _processed.RDS file from the apt/rcnorm pre-processing
+# Uses the copynumber::winsorize to winsorize L2R at smooth.k MADs
+# Alelle-specific piecewise fit segmentation using ASCAT::ascat.aspcf
+# Recenters based on the 'recenter' parameter:
+#   - l2r.centeredpeak (Default): Most centred peak in L2R density
+#   - l2r.mainpeak: Heighest peak of the L2R density
+#   - l2r.median: Median of L2R distribution
+# Winsorization of segmented L2R using a MAD k=5
+# Rescues small CN events using the changepoint::cpt.mean PELT method with
+# a penalization factor of 40
+#
 for(segmenter in c("ASCAT")){
   message("Running L2R segmentation...")
   
@@ -208,10 +227,22 @@ for(segmenter in c("ASCAT")){
                                      astep="L2R", segmenter=toupper(segmenter))
   RDS.files <- .splitSamples(RDS.files, opt$idx, opt$grpsize)
   
-  EaCoN:::Segment.ff.Batch(RDS.file = RDS.files,  segmenter = segmenter, nthread=2)
+  EaCoN::Segment.ff.Batch(RDS.file = RDS.files,  segmenter = segmenter, nthread=3)
 }
 
-#### ASCN Calls: ####
+########################
+#### 3) ASCN Calls: ####
+# Takes in the *.SEG.ASCAT.RDS file from the ASCAT aspcf analysis
+# Scans a gamma range of (gammaRange[1], gammaRange[2], by=0.05) (0.35, 0.95, 0.05)
+#  Gamma parameter: (https://www.crick.ac.uk/research/labs/peter-van-loo/software)
+#    The parameter represents the drop in LogR for a change from two copies to 
+#    one copy in 100% of cells. Gamma theoretically should equal 1, but due to 
+#    array background signal and bespoke array normalisation procedures, in 
+#    practice it is often significantly lower. Its default setting of 0.55 works 
+#    for many but not all SNP arrays
+# Runs ASCAT, ASCAT::ascat.runAscat, using the gamma parameter
+# Basic visualization and reports after this function
+# 
 for(segmenter in c("ASCAT")){
   message("Obtaining allele-specific copy number (ASCN) calls...")
   ## CN Estimation:
@@ -223,7 +254,7 @@ for(segmenter in c("ASCAT")){
   l2r.rds <- .splitSamples(l2r.rds, opt$idx, opt$grpsize)
   print(l2r.rds)
   
-  fit.val <- EaCoN:::ASCN.ff.Batch(RDS.files = l2r.rds, nthread=2, force=TRUE)
+  fit.val <- EaCoN::ASCN.ff.Batch(RDS.files = l2r.rds, nthread=4, force=TRUE)
 }
 
 
@@ -265,6 +296,11 @@ for(segmenter in c("ASCAT")){
                        meta <- gdsc.meta[,c('Sample Name', 'Cancer Type (matching TCGA label)')]
                        colnames(meta) <- c('sample', 'TCGA_code')
                        list('meta.tcga'=meta, 'meta'=gdsc.meta)
+                     },
+                     '1000G'={
+                       meta <- data.frame("sample"=names(all.fits),
+                                          "TCGA_code"=rep("Normal", length(all.fits)))
+                       list('meta.tcga'=meta, 'meta'=meta)
                      })
     
     data(pancanPloidy.noSegs)
@@ -280,7 +316,7 @@ for(segmenter in c("ASCAT")){
       start_time <- Sys.time()
       
       gr.cnv <- annotateRDS.Batch(all.fits[r['start']:r['end']], 
-                                  toupper(segmenter), nthread=3,
+                                  toupper(segmenter), nthread=4,
                                   gamma.method='score', gamma.meta=meta.l$meta.tcga,
                                   pancan.ploidy=pancan.ploidy, 
                                   feature.set=c('bins'),
@@ -303,11 +339,55 @@ for(segmenter in c("ASCAT")){
       
       end_time <- Sys.time()
       t1 <- end_time - start_time
-      print(paste0("Time to create jaccard matrix: ", round(t1,2), "s"))
-      
       gc()
-      r
     })
     
+  }
+}
+
+
+## Outdated? Maybe?
+qsub.split <- FALSE
+if(qsub.split){
+  if(file.exists(file.path("..", "scripts", "samples.RData"))){
+    EaCoN:::tmsg("Getting sample IDs...")
+    load(file.path("..", "scripts", "samples.RData"))
+    range.s <- seq(1, length(sample.ids), by=10)
+    range.e <- c((range.s + 9), length(sample.ids))
+    range.idx <- opt$idx
+    
+    range.se <- c(range.s[range.idx], range.e[range.idx])
+    print(paste0(range.idx, " : ", paste(range.se, collapse="-")))
+    
+    sample.ids <- sample.ids[range.se[1]:range.se[2]]
+    #sample.ids <- sample.ids[opt$idx]
+  } else {
+    # fit.vals.path <- list.files(pattern="gammaEval.txt", recursive=T, full=T)
+    # rm.idx <- unlist(sapply(sample.ids, function(x) any(grepl(x, fit.vals.path))))
+    
+    rm.idx <- sapply(sample.ids, function(s){
+      if(dir.exists(s)){
+        if(any(grepl("temp", list.files(s)))){
+          print(paste("Incomplete sample [temp present]:", s))
+          unlink(file.path(s), recursive = TRUE)
+          temp.idx <- FALSE
+        } else if(any(grepl("RDS$", list.files(s)))) {
+          temp.idx <- TRUE
+        } else {
+          print(paste("Incomplete sample [RDS missing]:", s))
+          unlink(file.path(s), recursive = TRUE)
+          FALSE
+        }
+      } else {
+        temp.idx <- FALSE
+      }
+    })
+    
+    #names(rm.idx) <- sample.ids
+    if(sum(rm.idx) > 0) sample.ids <- sample.ids[-which(rm.idx)]
+    #sample.ids <- sample.ids[sample(x = c(1:length(sample.ids)), size = length(sample.ids), replace = F)]
+    
+    EaCoN:::tmsg("Saving samples...")
+    save(sample.ids, file=file.path("..", "scripts", "samples.RData"))
   }
 }
